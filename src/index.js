3596d7196130676c7e943e01790798e4edb2b1c1
Dheeraj4103 <dheerajjadhav4455@gmail.com>
2022-08-12 18:57:03 +0530

    modified files

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import menuchange from './menu'
-import aboutchange from './about'
-import './index.css'
-
-
-
+import menuchange from "./menu";
+import aboutchange from "./about";
+import "./index.css";
 
 const content = document.getElementById("content");
 
-const title = document.getElementById('title');
+const title = document.getElementById("title");
 const navbar = document.getElementById("nav");
 
 const home = document.getElementById("home");
 home.onclick = () => {
-    window.location.reload();
-}
+  window.location.reload();
+};
 
 const menu = document.getElementById("menu");
 menu.onclick = () => {
-    content.innerHTML = "";
-    title.style.display = "none";
-    menuchange();
-}
+  content.innerHTML = "";
+  title.style.display = "none";
+  menuchange();
+};
 
-const about = document.getElementById('about');
+const about = document.getElementById("about");
 about.onclick = () => {
-    content.innerHTML = "";
-    title.style.display = "none";
-    aboutchange();
-}
-
-
-
+  content.innerHTML = "";
+  title.style.display = "none";
+  aboutchange();
+};
